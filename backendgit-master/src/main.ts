9b4f8c01db01d62a8d1c46166a2b5cb71ee12c38
It's me, Markus <markus.ge2003@gmail.com>
2026-03-08 15:41:20 +0700

    fix main_ts

--- backendgit-master/src/main.ts
+++ backendgit-master/src/main.ts
@@ -2,7 +2,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { hostname } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +11,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:5173',
       'http://localhost:5174',
-      process.env.FRONTEND_URL, // frontend domain ตอน deploy
+      process.env.FRONTEND_URL,
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
@@ -29,7 +28,9 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT || 4000;
-  await app.listen(port, hostname: '0.0.0.0');
+
+  // สำคัญสำหรับ deploy
+  await app.listen(port, '0.0.0.0');
 
   console.log(`API running on port ${port}`);
 }
